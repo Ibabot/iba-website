f81471621008a1e362d388c075b88c254a50ac83
Laura Orsini Franca <laura@stokkur.is>
2019-12-01 19:59:08 +0000

    Geolocation package added, initial skeleton

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -8,7 +8,7 @@ function ChatBox() {
     return (
       <Widget
         interval={2000}
-        socketUrl={"https://ibachatbot.herokuapp.com"}
+        socketUrl={"http://localhost:5006"}
         socketPath={"/socket.io/"}
         title={"Íba"}
         subtitle={"Íslandsbanki"}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ChatBox from '../components/ChatBox';
+import Geolocator from "../components/Geolocator";
 
 
 class Home extends Component {
     render() {
         return (
           <div className="App">
+            <Geolocator />
             <div className="chat-container">
               <ChatBox />
             </div>
