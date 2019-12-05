2686505651e3db90a69f17347a2cc11faf49ed47
Laura Orsini Franca <laura@stokkur.is>
2019-12-05 21:07:46 +0000

    Style changes, new image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import './header.scss';
-import logo from '../assets/img/isb-logo.svg';
+import logo from '../assets/img/isb-logo-red.svg';
 
 
 function Header() {
-    return(
-        <header className="web-header">
-            <div className="logo">
-                <img src={logo} alt="Íslandsbankinn logo" className="logo" />
-            </div>            
-            <input type="text" className="input"></input>
-        </header>
-    )
+    return (
+      <header className="web-header">
+        <div className="logo">
+          <img src={logo} alt="Íslandsbankinn logo" className="logo" />
+        </div>
+        <div className="web-header-text">
+          <h1>Say hello to Íba</h1>
+          <p className="bottom-space">
+            Íba is Íslandsbanki's new chatbot assistant
+          </p>
+          <a
+            className="btn btn-red"
+            href="https://www.islandsbanki.is/is/vara/thjonusta/hafa-samband"
+            target="_blank"
+          >
+            Be in touch
+          </a>
+        </div>
+      </header>
+    );
 }
 
 export default Header;
+
+{/* <a href="https://www.freepik.com/free-photos-vectors/technology">
+  Technology vector created by upklyak - www.freepik.com
+</a>; */}
